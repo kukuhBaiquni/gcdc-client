3d4420de63d0898f17a359771acda00b108b19b3
kukuhbaiquni <kukuh.baiquni@gmail.com>
2022-11-01 20:45:06 +0700

    nyoba

---
+++ src/pages/_app.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object,
+}
+
+export default MyApp
