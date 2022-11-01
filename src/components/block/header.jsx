8ebeb4ab2622d468c6a22aaa7556323e5a6c1bfb
kukuhbaiquni <kukuh.baiquni@gmail.com>
2022-11-02 01:29:00 +0700

    DARK MODE

--- src/components/block/header.jsx
+++ src/components/block/header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Image from 'next/image'
+import useDarkMode from 'hooks/use-dark-mode'
 
 export default function Header() {
+  const { theme, setTheme } = useDarkMode()
   return (
     <header className='w-full h-24 sticky left-0 top-0 drop-shadow-sm tm-base'>
-      ooos
+      <div className='max-w-screen-2xl mx-auto h-full items-center flex justify-between'>
+        <Image
+          alt='logo'
+          height={70}
+          src='/images/logo.png'
+          width={200}
+        />
+        <div>
+          {theme === 'dark' ? (
+            <i
+              className='bx bxs-sun text-2xl cursor-pointer'
+              onClick={() => setTheme('light')}
+            />
+          ) : (
+            <i
+              className='bx bxs-moon text-2xl cursor-pointer'
+              onClick={() => setTheme('dark')}
+            />
+          )}
+        </div>
+      </div>
     </header>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import DefaultLayout from 'layouts/default-layout'
 
 export default function Home() {
   return (
-    <div className='bg-pink-300'>
+    <div>
       TEsts
       <div className='h-10 w-10 rounded-full bg-sky-500' />
     </div>
